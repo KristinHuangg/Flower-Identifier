9df2d4506b952e42ba0b8e5587cf7a861f70eb50
KristinHuang <kristinhuangg@gmail.com>
2021-09-21 19:45:37 -0400

    App.js added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import FileUpload from './components/FileUpload';
+import * as mobilenet from "@tensorflow-models/mobilenet";
+import '@tensorflow/tfjs-backend-webgl';
+import {useState, useEffect} from 'react';
+import { set } from 'express/lib/application';
 
 function App() {
+  const [isModelLoading, setIsModelLoading] = useState(false);
+  const [model, setModel] = useState(null);
+  const [imageURL, setImageURL] = useState(null);
+  const [results, setResults] = useState([]);
+
+  const loadModel = async () => {
+    setIsModelLoading(true);
+    try {
+      const model = await mobilenet.load();
+      setModel(model);
+      setIsModelLoading(false);
+    } catch (error) {
+      console.log(error);
+      console.log("set model failed");
+      setIsModelLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    loadModel()
+  }, []) // load model only when app is being loaded
+
+  if (isModelLoading) {
+    return <h2>Model Loading...</h2>
+  }
+
+  console.log(imageURL);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="header">
+      <h1>Flower Identification</h1>
+      <FileUpload imageURL={imageURL} setImageURL={setImageURL} model={model}
+        results={results} setResults={setResults}></FileUpload>
     </div>
   );
 }
